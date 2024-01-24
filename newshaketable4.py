49cb52281b094821f9ae31574a81eba988fa18bd
sillypantscoder <78504018+sillypantscoder@users.noreply.github.com>
2024-01-24 20:02:41 +0000

    Uploaded original files
    This version is WORKING but still in progress. Use the "Motor Control" page if you need to move the motors.

---
+++ newshaketable4.py
@@ -0,0 +1,64 @@
+import time
+import serial
+import typing
+import sys
+
+crc = 0
+def crc_clear():
+	global crc
+def crc_update(data):
+	global crc
+	crc = crc ^ (data << 8)
+	for bit in range(0, 8):
+		if (crc&0x8000)  == 0x8000:
+			crc = ((crc << 1) ^ 0x1021)
+		else:
+			crc = crc << 1
+	return
+
+# Set up serial connection with the Roboclaw controller
+ser = serial.Serial('/dev/ttyS0', 38400, timeout=5)
+
+def writebyte(val):
+	crc_update(val)
+	ser.write(bytes([val]))
+def readbyte():
+	data = ser.read(1)
+	print(repr(data))
+	if len(data):
+		val = ord(data)
+		crc_update(val)
+		return (1, val)
+	return (0, 0)
+# Function to send a command to the Roboclaw controller
+def move_motor(motor: int = 1, speed: int = 64):
+	# sendcommand
+	crc_clear()
+	crc_update(0x80)
+	ser.write(bytes([0x80]))
+	cmd_int = 0 if speed >= 0 else 1
+	# Speed greater than 0 is forwards
+	# Less than 0 is backwards
+	cmd_int += [0, 4][motor - 1]
+	crc_update(cmd_int)
+	ser.write(bytes([cmd_int]))
+	# writebyte(val)
+	writebyte(abs(speed))
+	# writeword(crc & 0xFFFF)
+	wordwrite = crc & 0xFFFF
+	writebyte((wordwrite >> 8) & 0xFF)
+	writebyte(wordwrite & 0xFF)
+	return
+	val = readbyte()
+	if len(val) > 0:
+		if val[0]:
+			return True
+	return False
+
+# move_motor(-32)
+move_motor(int(sys.argv[1][0]), int(sys.argv[1][1:]))
+
+# input()
+# move_motor(0)
+
+ser.close()
---
+++ newshaketable4server_large.py
@@ -0,0 +1,245 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import typing
+import json
+import subprocess
+import math
+import os
+import threading
+import time
+
+hostName = "0.0.0.0"
+serverPort = 8080
+
+def read_file(filename):
+	f = open(filename, "r")
+	t = f.read()
+	f.close()
+	return t
+
+def bin_read_file(filename):
+	f = open(filename, "rb")
+	t = f.read()
+	f.close()
+	return t
+
+def write_file(filename, content):
+	f = open(filename, "w")
+	f.write(content)
+	f.close()
+
+class HttpResponse(typing.TypedDict):
+	status: int
+	headers: dict[str, str]
+	content: "str | bytes"
+
+def generate_file(filename: str, time: float, xMotion: float, yMotion: float):
+	points: list[tuple[float, float, float]] = []
+	for i in range(round(time * 2)):
+		x = xMotion * math.sin(i)
+		y = yMotion * math.sin(i + 1)
+		points.append((i / 2, x, y))
+	while os.path.exists("datas/" + filename + ".json"):
+		filename += "2"
+	f = open("datas/" + filename + ".json", "w")
+	f.write(json.dumps(points))
+	f.close()
+
+def set_motor_pos(xDiff: float, yDiff: float, duration: float):
+	speeds = [
+		round(xDiff * (1 / duration) * -400),
+		round(yDiff * (1 / duration) * -400)
+	]
+	if speeds[0] >= 255: speeds[0] = 255
+	if speeds[1] >= 255: speeds[1] = 255
+	if speeds[0] <= -255: speeds[0] = -255
+	if speeds[1] <= -255: speeds[1] = -255
+	subprocess.run(["python3", "newshaketable4.py", f"1{speeds[1]}"])
+	subprocess.run(["python3", "newshaketable4.py", f"2{speeds[0]}"])
+	time.sleep(duration)
+	subprocess.run(["python3", "newshaketable4.py", "10"])
+	subprocess.run(["python3", "newshaketable4.py", "20"])
+
+def run_file(data: list[tuple[float, float, float]]):
+	print(f"[0/{len(data)}]")
+	for i in range(len(data) - 1):
+		timeStart = data[i][0]
+		timeEnd = data[i + 1][0]
+		posStart = [
+			data[i][1],
+			data[i][2]
+		]
+		posEnd = [
+			data[i + 1][1],
+			data[i + 1][2]
+		]
+		timeDiff = timeEnd - timeStart
+		posDiff = [
+			posEnd[0] - posStart[0],
+			posEnd[1] - posStart[1]
+		]
+		# decrease: list[float] = [*posDiff]
+		# if posDiff[0] >= 256: decrease[0] += posDiff[0] - 255
+		# if posDiff[1] >= 256: decrease[1] += posDiff[1] - 255
+		set_motor_pos(posDiff[0] / 2, posDiff[1] / 2, timeDiff)
+		print(f"[{i + 1}/{len(data)}]")
+
+def start_running_file(data: str):
+	t: list[tuple[float, float, float]] = json.loads(data)
+	# t = [
+	# 	(i / 10, round(math.sin(0.5 * math.pi * i) * 0.01, 2), 0)
+	# 	for i in range(20)
+	# 	# (0,   0.5, 0.5),
+	# 	# (0.1, 0.4, 0.5),
+	# 	# (0.2, 0.4, 0.4),
+	# 	# (0.3, 0.6, 0.4),
+	# 	# (0.4, 0.5, 0.5)
+	# ]
+	# write_file("datas/sine.json", json.dumps(t))
+	threading.Thread(target=run_file, args=(t,)).start()
+
+def get(path: str) -> HttpResponse:
+	if path == "/":
+		return {
+			"status": 200,
+			"headers": {
+				"Content-Type": "image/svg+xml"
+			},
+			"content": read_file("client/home.xml")
+		}
+	elif path == "/motors":
+		return {
+			"status": 200,
+			"headers": {
+				"Content-Type": "image/svg+xml"
+			},
+			"content": read_file("client/motors.xml")
+		}
+	elif path == "/create":
+		return {
+			"status": 200,
+			"headers": {
+				"Content-Type": "image/svg+xml"
+			},
+			"content": read_file("client/create.xml")
+		}
+	elif path == "/run":
+		return {
+			"status": 200,
+			"headers": {
+				"Content-Type": "image/svg+xml"
+			},
+			"content": read_file("client/run.xml")
+		}
+	elif path.startswith("/set/run/"):
+		setname = path[9:-1]
+		contents = read_file("datas/" + setname + ".json")
+		start_running_file(contents)
+		return {
+			"status": 200,
+			"headers": {
+				"Content-Type": "image/svg+xml"
+			},
+			"content": read_file("client/watch.xml").replace("{{FILENAME}}", setname).replace("{{FILEINFO}}", contents)
+		}
+	elif path.startswith("/set/"):
+		setname = path[5:-1]
+		contents = read_file("datas/" + setname + ".json")
+		return {
+			"status": 200,
+			"headers": {
+				"Content-Type": "image/svg+xml"
+			},
+			"content": read_file("client/view.xml").replace("{{FILENAME}}", setname).replace("{{FILEINFO}}", contents)
+		}
+	elif path == "/data/ls":
+		return {
+			"status": 200,
+			"headers": {
+				"Content-Type": "text/plain"
+			},
+			"content": "\n".join([x[:-5] for x in os.listdir("datas")])
+		}
+	else: # 404 page
+		return {
+			"status": 404,
+			"headers": {
+				"Content-Type": "text/html"
+			},
+			"content": f""
+		}
+
+def post(path: str, body: bytes) -> HttpResponse:
+	if path == "/motors":
+		subprocess.run(["python3", "newshaketable4.py", body])
+		return {
+			"status": 200,
+			"headers": {},
+			"content": f""
+		}
+	elif path == "/create":
+		info = json.loads(body.decode("UTF-8"))
+		generate_file(info["filename"], info["time"], info["xMotion"], info["yMotion"])
+		return {
+			"status": 200,
+			"headers": {},
+			"content": f""
+		}
+	elif path == "/move_motor":
+		info = [float(x) for x in body.decode("UTF-8").split("\n")]
+		dist = math.dist(info, (0, 0))
+		threading.Thread(target=set_motor_pos, args=(*info, dist * 7)).start()
+		return {
+			"status": 200,
+			"headers": {},
+			"content": f""
+		}
+	else:
+		return {
+			"status": 404,
+			"headers": {
+				"Content-Type": "text/html"
+			},
+			"content": f""
+		}
+
+class MyServer(BaseHTTPRequestHandler):
+	def do_GET(self):
+		global running
+		res = get(self.path)
+		self.send_response(res["status"])
+		for h in res["headers"]:
+			self.send_header(h, res["headers"][h])
+		self.end_headers()
+		c = res["content"]
+		if isinstance(c, str): c = c.encode("utf-8")
+		self.wfile.write(c)
+	def do_POST(self):
+		res = post(self.path, self.rfile.read(int(self.headers["Content-Length"])))
+		self.send_response(res["status"])
+		for h in res["headers"]:
+			self.send_header(h, res["headers"][h])
+		self.end_headers()
+		c = res["content"]
+		if isinstance(c, str): c = c.encode("utf-8")
+		self.wfile.write(c)
+	def log_message(self, format: str, *args) -> None:
+		return;
+		if 400 <= int(args[1]) < 500:
+			# Errored request!
+			print(u"\u001b[31m", end="")
+		print(args[0].split(" ")[0], "request to", args[0].split(" ")[1], "(status code:", args[1] + ")")
+		print(u"\u001b[0m", end="")
+		# don't output requests
+
+if __name__ == "__main__":
+	running = True
+	webServer = HTTPServer((hostName, serverPort), MyServer)
+	webServer.timeout = 1
+	print("Server started http://%s:%s" % (hostName, serverPort))
+	while running:
+		try:
+			webServer.handle_request()
+		except KeyboardInterrupt:
+			running = False
+	webServer.server_close()
+	print("Server stopped")
